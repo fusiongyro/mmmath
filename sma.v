32289f6a9c30f22b91c0474487b740d4c08e22a6
Daniel K Lyons <fusion@storytotell.org>
2018-04-20 10:24:45 -0600

    Trying to turn it into a class and failing; syntatic improvements

--- sma.v
+++ sma.v
@@ -5,21 +5,30 @@ Variable sum : D -> D -> D.
 Variable product : D -> D -> D.
 Infix "+" := sum.
 Infix "*" := product.
+Axiom commutative_p : forall (a b : D), a + b = b + a.
+
+(* Class IntegralDomain D : Type := {
+ sum : D -> D -> D;
+ product : D -> D -> D;
+ Notation "x + y" := sum x y;
+ Infix "+" := sum;
+ Infix "*" := product;
+}. *)
 
 (* Axiom closure : forall a, b : D => a + b \in D, a * b \in D *)
 (* Axiom uniqueness_s : forall a, b : D, a = a', b = b' => a + b = a' + b'. *)
-Axiom commutative_p : forall a : D, forall b : D, a + b = b + a.
-Axiom commutative_m : forall a : D, forall b : D, a * b = b * a.
+Axiom commutative_m : forall a b : D, a * b = b * a.
 
-Axiom associative_p : forall a : D, forall b : D, forall c : D, a + (b + c) = (a + b) + c.
-Axiom associative_m : forall a : D, forall b : D, forall c : D, a * (b * c) = (a * b) * c.
+Axiom associative_p : forall a b c : D, a + (b + c) = (a + b) + c.
+Axiom associative_m : forall a b c : D, a * (b * c) = (a * b) * c.
 
-Axiom distributive : forall a : D, forall b : D, forall c : D, a * (b + c) = a * b + a * c.
+Axiom distributive : forall a b c : D, a * (b + c) = a * b + a * c.
 
 Axiom zero : exists zero : D, forall a : D, a + zero = a.
 
 Axiom unity : exists one : D, forall a : D, a * one = a.
-
+(* } *)
+                         
 Theorem abc : forall a : D, forall b : D, forall c : D,
         a + b + c = c + b + a.
 Proof.
